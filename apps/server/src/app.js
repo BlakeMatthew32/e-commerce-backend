826333e79ed4dc2ec40dbf31ec75068f65b28e25
Blake Matthew <79722560+BlakeMatthew32@users.noreply.github.com>
2025-10-23 22:33:01 +0100

    feat: work on registration endpoint

--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -6,10 +6,27 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get('/products', async (req, res, next) => {
   const result = await db.query('SELECT * FROM products LIMIT 10;');
   res.send(result.rows);
-})
+});
+
+app.post('/users/register', async (req, res, next) => {
+  const { firstName, lastName, email, password, passwordConfirm } = req.body;
+
+  const userExists = await db.checkUserExists(email);
+
+  if (userExists) {
+    res.send(`This user already exists.`);
+    return;
+  };
+
+  db.createUser(firstName, lastName, email, password);
+
+  res.send(`User Exists: ${userExists}`);
+});
 
 app.listen(PORT, (error) => {
   if(!error) {
---
+++ apps/server/src/db/index.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+dotenv.config({debug: true});
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+
+const query = (text, params) => {
+  return pool.query(text, params);
+};
+
+const checkUserExists = async (userEmail) => {
+  const results = await pool.query('SELECT * FROM customers WHERE email = $1', [userEmail]);
+  return (results.rows[0] ? true : false);
+};
+
+const createUser = async (firstName, lastName, email, password) => {
+  const results = await pool.query('INSERT INTO customers (first_name, last_name, email) VALUES ($1, $2, $3);', [firstName, lastName, email]);
+}
+
+export { query, checkUserExists, createUser }
